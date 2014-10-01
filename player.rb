27d0c011f5e0c6e50dcf1c328147ad9725ecc681
Rod McLaughlin <rodmclaughlin@gmail.com>
2014-09-30 20:13:07 -0700

    Various improvements regarding reading players from csv file

--- player.rb
+++ player.rb
@@ -1,24 +1,30 @@
 require 'fastercsv'
+require 'csv'
 
 class Player
 
-# playerID,yearID,league,teamID,G,AB,R,H,2B,3B,HR,RBI,SB,CS
+  ATTRIBUTES = [:playerID, :yearID, :league, :teamID, :G, :AB, :R, :H, :B2, :B3, :HR, :RBI, :SB, :CS]
+  ATTRIBUTES.each { |attribute| attr_accessor attribute }  
 # accarje01,2012,AL,CLE,26,0,0,0,0,0,0,0,0,0
 # accarje01,2012,AL,OAK,1,,,,,,,,,
 # accarje01,2011,AL,BAL,1,0,0,0,0,0,0,0,0,0
 # accarje01,2009,AL,TOR,26,0,0,0,0,0,0,0,0,0
 # accarje01,2008,AL,TOR,16,,,,,,,,,
 
-
   def initialize csv
-puts csv
+    raise "Don't use the first line of a CSV file to initialize a player" if csv[ 0 ] == 'playerID'
+    csv.size.times do |t|
+      value = csv[ t ]
+      send( (ATTRIBUTES[ t ].to_s + '=').to_sym, value )
+    end
   end
 
   def Player.all
     csvs = CSV.read( File.expand_path( '../Batting-07-12.csv', __FILE__ ))
     players = []
     csvs.each do |csv|
-      players << Player.new csv
+      next if csv[0] == 'playerID'
+      players << Player.new( csv )
     end
     players
   end
