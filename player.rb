77080443814f547a9055a41e7d8b69d915dc9ece
Rod McLaughlin <rodmclaughlin@gmail.com>
2014-09-30 19:35:50 -0700

    Adding basic tests for non-existent methods which the project depends on

--- player.rb
+++ player.rb
@@ -1,7 +1,30 @@
+require 'fastercsv'
+
 class Player
 
 # playerID,yearID,league,teamID,G,AB,R,H,2B,3B,HR,RBI,SB,CS
+# accarje01,2012,AL,CLE,26,0,0,0,0,0,0,0,0,0
+# accarje01,2012,AL,OAK,1,,,,,,,,,
+# accarje01,2011,AL,BAL,1,0,0,0,0,0,0,0,0,0
+# accarje01,2009,AL,TOR,26,0,0,0,0,0,0,0,0,0
+# accarje01,2008,AL,TOR,16,,,,,,,,,
 
+
+  def Player.all
+    players = []
+    csvs.each do |csv|
+      players << Player.init csv
+    end
+    players
+  end
+
+  def eql?(other)
+    other.instance_of?(self.class) && self.playerID == other.playerID 
+  end
+
+  def ==(other)
+    self.eql?(other)
+  end
 
 end
 
--- spec/baseball_spec.rb
+++ spec/baseball_spec.rb
@@ -1,13 +1,23 @@
-require File.expand_path( '../../player', __FILE__)
+require File.expand_path( '../../player', __FILE__ )
+
+RANGE = ( 2009 .. 2010 )
 
 describe 'baseball' do
+  
+  context 'basic methods' do
+    players = Player.all
+    expect(players[0]).not_to eq players[-1]
+    players.each do |player|
+      expect(player.respond_to? :at_bats).to be_true
+      expect(player.least_improved_batting_average( RANGE )).not_to be_nil 
+      expect(player.most_improved_batting_average( RANGE )).not_to be_nil
+    end
+  end
 
   context 'improved batting average' do
 
-    RANGE = ( 2009 .. 2010 )
-
     it 'should output the most improved batting average (hits/at-bats) from 2009-2010 for players with at least 200 at-bats' do
-      least_improved = Player.all.select{ |player| player.least_improved_batting_average( RANGE ) }
+      least_improved = Player.all.select{ |player| player.least_improved_batting_average( RANGE ) > 200 }
       at_least_200 = Player.all.select { |dude| dude.at_bats > 199 } 
       expect(least_improved.size).to be > 0
       expect(at_least_200.size).to be > 0
