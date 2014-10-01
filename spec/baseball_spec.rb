a192ba20aebccc9a100b5b16f482fab3c8e65c25
Rod McLaughlin <rodmclaughlin@gmail.com>
2014-09-30 18:56:53 -0700

    First spec - no assertions yet

---
+++ spec/baseball_spec.rb
@@ -0,0 +1,14 @@
+describe 'baseball' do
+
+  context 'improved batting average' do
+
+    it 'should output the most improved batting average (hits/at-bats) from 2009-2010 for players with at least 200 at-bats' do
+
+
+    end
+
+  end
+
+end
+
+
