38b1586d7fe4876b3be5e5b4e58e53e98ce70a61
Rod McLaughlin <rodmclaughlin@gmail.com>
2014-09-30 23:46:08 -0700

    Close to getting spec passing

--- spec/baseball_spec.rb
+++ spec/baseball_spec.rb
@@ -64,12 +64,12 @@ describe 'baseball' do
     end
 
     it 'should output the most improved batting average (hits/at-bats) from 2009-2010 for players with at least 200 at-bats' do
-      least_improved = Player.all.select{ |player| player.least_improved_batting_average( RANGE ) > 200 }
+      least_improved = Player.all.select{ |player| player.least_improved_batting_average( RANGE ) > 10 }
       at_least_200 = Player.all.select { |dude| dude.at_bats > 199 } 
       expect(least_improved.size).to be > 0
       expect(at_least_200.size).to be > 0
       at_least_200.each do |winner|
-        expect(least_improved.include? winner).to_be false
+        expect(least_improved).not_to include winner
       end
       
       at_least_200.each do |player|
