b6e45299174adb4094e420611e5f4c4dc0cd1a6c
Rod McLaughlin <rodmclaughlin@gmail.com>
2014-09-30 21:08:55 -0700

    Still getting nil effor in add in baseball spec line 15

--- player.rb
+++ player.rb
@@ -5,24 +5,24 @@ class Player
 
   @@players = []
 
+# playerID,  yearID, league, teamID, G, AB, R, H, 2B, 3B, HR, RBI, SB, CS
+# accarje01, 2011,   AL,     BAL,    1, 0,  0, 0, 0,  0,  0,  0,   0,  0
   ATTRIBUTES = [:playerID, :yearID, :league, :teamID, :G, :AB, :R, :H, :B2, :B3, :HR, :RBI, :SB, :CS]
   ATTRIBUTES.each { |attribute| attr_accessor attribute }  
-# accarje01,2012,AL,CLE,26,0,0,0,0,0,0,0,0,0
-# accarje01,2012,AL,OAK,1,,,,,,,,,
-# accarje01,2011,AL,BAL,1,0,0,0,0,0,0,0,0,0
-# accarje01,2009,AL,TOR,26,0,0,0,0,0,0,0,0,0
-# accarje01,2008,AL,TOR,16,,,,,,,,,
 
   def initialize csv
     raise "Don't use the first line of a CSV file to initialize a player" if csv[ 0 ] == 'playerID'
+    raise "The argment to new should be an array of size #{ ATTRIBUTES.size }" unless csv.size == ATTRIBUTES.size
     csv.size.times do |t|
       value = csv[ t ]
-      value = '0' if value.nil? # see empty data above ... TOR,16,,,,,,,,,
+      value = '0' if value.nil? 
       send( (ATTRIBUTES[ t ].to_s + '=').to_sym, value )
     end
+    @@players << self
   end
 
   def add csv
+    raise "The argment to add should be an array of size #{ ATTRIBUTES.size }" unless csv.size == ATTRIBUTES.size
     ab = self.AB.to_i
     ab += csv[ 5 ].to_i
     self.AB = ab.to_s 
